ae0d8f080502c137c87de250778514201b74f10a
Bryan Alejandro Sanchez Ramirez <sarabryanale@outlook.com>
2020-12-17 19:36:28 -0600

    NEW: Adding dotnev support

---
+++ src/config/index.ts
@@ -0,0 +1,15 @@
+import dotnet from 'dotenv';
+import path from 'path';
+
+// Get env path
+const envPath = path.resolve(__dirname, `${process.env.ENV_FILE_PATH}`);
+
+// Load variables into .env file
+dotnet.config({ path: envPath });
+
+// Assign variables into object
+const config = {
+  // test: process.env.CONFIG_TEST || ""
+};
+
+export default config;
